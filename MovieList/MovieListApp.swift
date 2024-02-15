5ded47da40bdb82e672909c9febe282a17ad0db7
Ikramuzzaman Muntasir <30627757+muntasir14@users.noreply.github.com>
2024-02-16 04:26:10 +0600

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Ikramuzzaman Muntasir on 16/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
